5bc14c960953919591aa6c0f6384e124b004ea1f
Matthew Parker <matt.parker1007@gmail.com>
2021-04-02 00:01:01 -0500

    saving and retrieving from local storage, and filling in text box values complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,47 @@
 var saveButtonEls = $('.saveBtn');
 var textBoxEls = $('textarea');
-var activeContents = [
-  {
-    hour: 9,
-    contents: ""
-  },
-  {
-    hour: 10,
-    contents: ""
-  },
-  {
-    hour: 11,
-    contents: ""
-  },
-  {
-    hour: 12,
-    contents: ""
-  },
-  {
-    hour: 13,
-    contents: ""
-  },
-  {
-    hour: 14,
-    contents: ""
-  },
-  {
-    hour: 15,
-    contents: ""
-  },
-  {
-    hour: 16,
-    contents: ""
-  },
-  {
-    hour: 17,
-    contents: ""
-  }
-];
+var planner = JSON.parse(localStorage.getItem('planner'));
+console.log(planner);
+if(!planner){
+  planner = [
+    {
+      hour: 9,
+      contents: ""
+    },
+    {
+      hour: 10,
+      contents: ""
+    },
+    {
+      hour: 11,
+      contents: ""
+    },
+    {
+      hour: 12,
+      contents: ""
+    },
+    {
+      hour: 13,
+      contents: ""
+    },
+    {
+      hour: 14,
+      contents: ""
+    },
+    {
+      hour: 15,
+      contents: ""
+    },
+    {
+      hour: 16,
+      contents: ""
+    },
+    {
+      hour: 17,
+      contents: ""
+    }
+  ];
+}
 
 
 
@@ -45,20 +49,27 @@ $('.saveBtn').on('click', function(event) {
   event.stopPropagation();
   var buttonEl = $(event.target);
   var inputEl = buttonEl.parent().prev().children().first();
-  
-  console.log(inputIndex);
   var todo = {
     hour: inputEl.attr('data-hour'),
     contents: inputEl.val()
   }
 
-  
+  for(var i = 0; i < planner.length; i++){   
+    if(planner[i].hour == todo.hour){
+      planner[i].contents = todo.contents;
+    }
+  }
+  localStorage.setItem('planner', JSON.stringify(planner));
+
 })
 
 
-// color the boxes based on current time
+// color the boxes based on current time; fill in saved text content
 var currentHour = moment().format('k');
 for(i=0; i<textBoxEls.length; i++){
+  $(textBoxEls[i]).val(planner[i].contents);
+
+
   var rowHour = i + 9; // first box represents 09:00, so add hour each iteration
   if(currentHour > rowHour){
     $(textBoxEls[i]).addClass('past');
